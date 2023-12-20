809f64961cc40b8b343943753769374f8915e69c
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2023-12-20 18:08:22 -0500

    single gaussian

--- src/experiments/mcmc/gaussian_single.jl
+++ src/experiments/mcmc/gaussian_single.jl
@@ -1,36 +1,33 @@
-# Source: https://www.nber.org/system/files/working_papers/w26340/w26340.pdf
-
 begin
     # Random.seed!(42)
 
-    num_sol = 10
+    set_default_plot_size(10 * inch, 8 * inch)
 
-    d = 10
-    F(θ) = sum(
-        idx -> 100 * (θ[idx+1] - θ[idx]^2)^2 + (1 - θ[idx])^2,
-        1:d-1
-    ) + 1
-    answer = ones(d)
+    d = 1
+    μ = -10
+    σ = 3
+    dist = MvNormal(fill(μ, d), I(d) * σ)
+    P(θ) = pdf(dist, θ)
+    S(θ) = -log(P(θ))
+    from, to = μ - 15, μ + 15
+
+    num_sol = 50
 
     result = @time rmc(
-        F, d, num_sol,
-        eta=1e-5,
-        isobjective=true,
-        θ_start=sample_hypersphere_surface( # adversarial start point
-            d,
-            radius=5,
-            center=answer
-        ),
+        P, d, num_sol,
+        g=6e-3,
+        m=1.0,
+        ϵ=0.995,
+        η=1e-5,
+        Δ=1e-3,
     )
 
     log_summary(result)
-
-    solution = argmin(F, result.solutions)
-
-    # solution should be ~= 1 @ ones(d)
-    @info "solution" repr(solution) F(solution)
-
-    if d == 2
-        plot_objective(F, xmin=-2, xmax=2, ymin=-1, ymax=2)
-    end
+    @info "std" std(result.accepted)[1]
+    
+    vstack(
+        # plot_trajectory(result, S, from=from, to=to),
+        plot_samples_1d(result, P, from=from, to=to)
+        # plot(x=1:length(result.speeds), y=result.speeds, Geom.line)
+    )
 end
