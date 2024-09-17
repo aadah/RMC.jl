c76b19666567e164e4094799ebb3a64aa6dc809a
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2024-09-17 12:58:21 -0400

    better logging

--- src/exp.jl
+++ src/exp.jl
@@ -8,7 +8,7 @@ function all_hyperparams()
 end
 
 function nlopt_grid_search(
-    E::Function, d::Integer;
+    F::Function, d::Integer;
     num_solutions::Integer=10,
     θ_start::Union{Nothing,Vector}=nothing,
     constraints::Union{Nothing,Vector}=nothing,
@@ -21,27 +21,28 @@ function nlopt_grid_search(
     for (i, (g, m, ϵ, η, Δ)) in enumerate(all_hyperparams())
         result = nothing
         try
-            print("$i\t")
-            result = @time rmc(
-                E, d, num_solutions;
+            stats = @timed rmc(
+                F, d, num_solutions;
                 g=g, m=m, ϵ=ϵ, η=η, Δ=Δ,
                 θ_start=θ_start,
                 constraints=constraints,
                 isobjective=true,
             )
-        catch e
-            @warn e g m ϵ η Δ
+            result = stats.value
+            @info "Experiment $i" elapsed_seconds=stats.time
+        catch error
+            @error "Experiment $i" error g m ϵ η Δ
             continue
         end
 
-        solution = argmin(s -> E(s.answer), result.solutions)
-        val = E(solution.answer)
+        solution = argmin(s -> F(s.answer), result.solutions)
+        val = F(solution.answer)
         if isnothing(best)
             # initial case
             best = (solution, result.hyperparams, val)
         elseif val ≈ best[3] && solution.evaluations < best[1].evaluations
             # special case: more or less found the minimum, but want to see if
-            # we did it in fewer evalutions of E
+            # we did it in fewer evalutions of F
             best = (solution, result.hyperparams, val)
         elseif val < best[3]
             # obvious case: found a better minimum
