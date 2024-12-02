ef54304dd157c3edfe14a61c37cc796f325f3012
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2024-12-02 00:26:28 -0500

    move experiments folder

---
+++ experiments/mcmc/normal.jl
@@ -0,0 +1,20 @@
+import RMC
+
+using Distributions
+
+begin
+    d = 1
+
+    dist = Normal(1, 2)
+    E(θ) = logpdf(dist, θ[1])
+
+    best = RMC.mcmc_grid_search(
+        E, d,
+        islogenergy=true,
+        target=dist,
+        random_start_fn=_ -> [rand(dist)],
+        saveto="$(@__FILE__).csv"
+    )
+
+    @info "result" best
+end
